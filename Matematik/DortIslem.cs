724d1cac715504cfdc7d7776ff5889de2b2ac260
gusta90 <gokhanusta90@gmail.com>
2021-01-20 23:57:56 +0300

    20 Ocak Ders Sonu

--- Matematik/DortIslem.cs
+++ Matematik/DortIslem.cs
@@ -6,10 +6,31 @@ namespace Matematik
 {
     class DortIslem
     {
-        public void Topla(int sayi1, int sayi2) 
+        public void Topla(int sayi1, int sayi2)
         {
             int toplam = sayi1 + sayi2;
             Console.WriteLine("Sonuç : " + toplam);
+
         }
+
+        public void Cikar(int sayi1, int sayi2)
+        {
+            int cikar = sayi1 - sayi2;
+            Console.WriteLine("Sonuç : " + cikar);
+        }
+
+        public void Carp(int sayi1, int sayi2)
+        {
+            int carpim = sayi1 * sayi2;
+            Console.WriteLine("Sonuç : " + carpim);
+        }
+
+        public void Bol(int sayi1, int sayi2)
+        {
+            decimal bolum = sayi1 / sayi2;
+            Console.WriteLine("Sonuç : " + bolum);
+        }
+
+
     }
 }
--- Matematik/Program.cs
+++ Matematik/Program.cs
@@ -10,6 +10,12 @@ namespace Matematik
             dortIslem.Topla(5, 6);
 
             dortIslem.Topla(6, 9);
+
+            dortIslem.Cikar(9, 12);
+
+            dortIslem.Carp(9, 12);
+
+            dortIslem.Bol(12,5);
         }
     }
 }

447a1e6d5383b69156dc2493cb459097493f4955
gusta90 <gokhanusta90@gmail.com>
2021-01-24 14:18:36 +0300

    24 Ocak 2021 Ders Sonu

---
+++ OOP3/IKrediManager.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOP3
+{
+    
+    //İnterface birbirinin alternatifi olan ve kod içerikleri farklı olan durumlar için kullanırız.
+    interface IKrediManager
+    {
+        void Hesapla();
+        void BiseyYap();
+      
+    }
+}
